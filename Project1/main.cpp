b1461f9528aa5a843fce652f513348778d24149e
jianye <18817571704@163.com>
2020-11-06 23:00:18 +0100

    'Project1'

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-char square[10] = {'0','1','2','3','4','5,','7','8','9'};
+char square[10] = {'0','1','2','3','4','5','6' ,'7', '8', '9'};
 
-int checkwin();
-void board();
+// int checkwin();
+// void board();
 
 int main(){
 
