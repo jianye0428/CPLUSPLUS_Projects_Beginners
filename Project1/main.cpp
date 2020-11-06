5f1de319dbc85d6cf287efea9c7336a987fd9ac1
jianye <18817571704@163.com>
2020-11-06 22:56:10 +0100

    'Project1'

---
+++ Project1/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+char square[10] = {'0','1','2','3','4','5,','7','8','9'};
+
+int checkwin();
+void board();
+
+int main(){
+
+    cout <<"Hello World"<<endl;
+    return 0;
+}
