7d16ac3798ff4c8ba9d572d09766cfb6326f693a
jianye <18817571704@163.com>
2020-11-07 11:09:41 +0100

    'Project 1'

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,11 +3,22 @@ using namespace std;
 
 char square[10] = {'0','1','2','3','4','5','6' ,'7', '8', '9'};
 
-// int checkwin();
-// void board();
+int checkwin();
+void board();
 
 int main(){
 
-    cout <<"Hello World"<<endl;
-    return 0;
+    int player = 1, i, choice;
+    char mark;
+    do{
+        board();
+        player = (player%2)?1:2;
+
+        cout << "Player" << player << ", enter a number: ";
+        cin >> choice;
+
+        mark=(player==1)?'X':'O';
+        
+
+    }(i==-1)
 }
